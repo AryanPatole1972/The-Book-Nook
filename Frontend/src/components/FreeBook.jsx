420b74ec1095d132b3f66a6c239d6ccf73481072
AryanPatole1972 <162153650+AryanPatole1972@users.noreply.github.com>
2025-12-22 11:07:20 +0530

    Update API endpoint for fetching free books

--- Frontend/src/components/FreeBook.jsx
+++ Frontend/src/components/FreeBook.jsx
@@ -13,7 +13,7 @@ function FreeBook() {
   useEffect(() => {
     const getBook = async()=>{
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://the-book-nook-baackend.onrender.com/book");
         const data = res.data.filter((data) => data.category === "Free");
         console.log(res.data)
         setBook(data);
