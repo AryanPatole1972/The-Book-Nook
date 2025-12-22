3d39338e16795a628ee901b08be2e974dfc809ed
AryanPatole1972 <162153650+AryanPatole1972@users.noreply.github.com>
2025-12-22 11:23:38 +0530

    Update Logout.jsx

--- Frontend/src/components/Logout.jsx
+++ Frontend/src/components/Logout.jsx
@@ -1,27 +1,28 @@
-<<<<<<< HEAD
 import React from "react";
 import { useAuth } from "../context/AuthProvider";
 import toast from "react-hot-toast";
 
 function Logout() {
   const [authUser, setAuthUser] = useAuth();
+
   const handleLogout = () => {
     try {
       setAuthUser({
         ...authUser,
         user: null,
       });
+
       localStorage.removeItem("Users");
-      toast.success("Logout successfully");
+      toast.success("Logged out successfully");
 
       setTimeout(() => {
         window.location.reload();
-      }, 3000);
+      }, 1000);
     } catch (error) {
-      toast.error("Error: " + error);
-      setTimeout(() => {}, 2000);
+      toast.error("Logout failed");
     }
   };
+
   return (
     <div>
       <button
@@ -35,30 +36,5 @@ function Logout() {
 }
 
 export default Logout;
-=======
-import React from 'react'
-import { useAuth } from '../context/AuthProvider'
 
-export default function Logout() {
-    const [authUser, setAuthUser] = useAuth()
-
-    const handleLogout=()=>{
-        try {
-            setAuthUser({
-                ...authUser,
-                user:null
-            })
-            localStorage.removeItem("Users")
-            alert("Loggedout")
-            window.location.reload();
-        } catch (error) {
-            alert("Error:"+error.message)
-        }
-    }
-  return (
-    <div>
-      <button className='px-3 py-2 bg-red-500 text-white rounded-md cursor-pointer' onClick={handleLogout}>Logout</button>
-    </div>
-  )
-}
->>>>>>> 638cb71217d54321e2ed4a2c054c950148837bbe
+            
